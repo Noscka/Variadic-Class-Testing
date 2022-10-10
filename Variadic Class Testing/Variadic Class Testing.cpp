2930c1b9c27a366db52c9f2b95e54fdbeaeaa540
Noscka <Adam882200@gmail.com>
2022-10-11 00:09:10 +0100

    Kinda close to creating a variadic class

--- Variadic Class Testing/Variadic Class Testing.cpp
+++ Variadic Class Testing/Variadic Class Testing.cpp
@@ -2,6 +2,35 @@
 #include <fcntl.h>
 #include <io.h>
 #include <string>
+#include <functional>
+
+/*
+TODO:
+- Make the constructor run the loading screen (later make it the StartLoading) if possible
+*/
+
+class Foo
+{
+    std::function<void(Foo*)> callback;
+
+public:
+    std::wstring String;
+
+    template <typename F, typename ... ArgsT>
+    Foo(F&& callable, ArgsT&& ... args) : callback([=](Foo* var) { callable(var, args...); })
+    {}
+
+    void RunFoo()
+    {
+        callback(this, L"Innit");
+    }
+};
+
+void function(Foo* param, std::wstring input)
+{
+    std::wcout << L"Running" << std::endl;
+    param->String = input;
+}
 
 void SomeFunction(std::wstring ArgInput)
 {
@@ -14,35 +43,12 @@ void PassToFunction(void (*Function)(VariadicArgs&& ...), VariadicArgs ... Args)
     return (Args + ...);
 }
 
-bool has_dups(int a[], int n)
-{
-    //assume no duplicates in the array
-    int has_duplicates = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        //variable to track the number of occurences of the current i element in a[]
-        int number_of_occurences = 0;
-
-        for (int j = 0; j < n; j++)
-        {
-            if (a[i] == a[j] && i != j)
-            {
-                has_duplicates = 1;
-                return has_duplicates;
-            }
-        }
-    }
-    return has_duplicates;
-}
-
 int main()
 {
     _setmode(_fileno(stdout), _O_U16TEXT);
 
-    int abc[]{1, 2, 2, 4, 5};
-
-    std::wcout << (has_dups(abc, 5) ? L"Has Dupes" : L"Hasn't dupes") << std::endl;
-
+    Foo FooTing(&function, L"Input");
+    wprintf(FooTing.String.c_str());
+    FooTing.RunFoo();
     return 0;
 }
