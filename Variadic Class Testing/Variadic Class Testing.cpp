dd5d4115aeb33fbaaa17a2f33399ee38559670d7
Adam Krawczyk <Adam882200@gmail.com>
2022-10-11 08:43:15 +0100

    Fully working variadic class passthrough

--- Variadic Class Testing/Variadic Class Testing.cpp
+++ Variadic Class Testing/Variadic Class Testing.cpp
@@ -2,53 +2,31 @@
 #include <fcntl.h>
 #include <io.h>
 #include <string>
-#include <functional>
-
-/*
-TODO:
-- Make the constructor run the loading screen (later make it the StartLoading) if possible
-*/
 
 class Foo
 {
-    std::function<void(Foo*)> callback;
-
 public:
     std::wstring String;
 
     template <typename F, typename ... ArgsT>
-    Foo(F&& callable, ArgsT&& ... args) : callback([=](Foo* var) { callable(var, args...); })
-    {}
-
-    void RunFoo()
+    Foo(F&& callable, ArgsT&& ... args)
     {
-        callback(this, L"Innit");
+        (*callable)(this, std::forward<ArgsT>(args)...);
     }
 };
 
-void function(Foo* param, std::wstring input)
+void function(Foo* param, std::wstring input, int SomeTing)
 {
     std::wcout << L"Running" << std::endl;
+    std::wcout << L"Number: " << SomeTing << std::endl;
     param->String = input;
-}
-
-void SomeFunction(std::wstring ArgInput)
-{
-    std::wcout << ArgInput << std::endl;
-}
-
-template<typename ... VariadicArgs>
-void PassToFunction(void (*Function)(VariadicArgs&& ...), VariadicArgs ... Args)
-{
-    return (Args + ...);
 }
 
 int main()
 {
     _setmode(_fileno(stdout), _O_U16TEXT);
 
-    Foo FooTing(&function, L"Input");
+    Foo FooTing(&function, L"Input", 2);
     wprintf(FooTing.String.c_str());
-    FooTing.RunFoo();
     return 0;
 }
