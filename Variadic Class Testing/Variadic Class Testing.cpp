5bc75ceb19263744f4069dff5d7733bcbe208667
Noscka <Adam882200@gmail.com>
2022-10-12 19:27:37 +0100

    Not Working

--- Variadic Class Testing/Variadic Class Testing.cpp
+++ Variadic Class Testing/Variadic Class Testing.cpp
@@ -3,6 +3,7 @@
 #include <io.h>
 #include <string>
 #include <thread>
+#include <functional>
 
 template <typename F, typename ... ArgsT>
 void StartThread(F&& callable, ArgsT&& ... args)
@@ -10,32 +11,64 @@ void StartThread(F&& callable, ArgsT&& ... args)
     (*callable)(this, std::forward<ArgsT>(args)...);
 }
 
-void function(Foo* param, std::wstring input, int SomeTing)
-{
-    std::wcout << L"Running" << std::endl;
-    std::wcout << L"Number: " << SomeTing << std::endl;
-    param->String = input;
-}
-
 class Foo
 {
 public:
     std::wstring String;
 
-    template <typename F, typename ... ArgsT>
-    Foo(F&& callable, ArgsT&& ... args)
+    template <typename F>
+    Foo(F* callable)
     {
-        std::thread(function, std::forward<ArgsT>(args)...);
+        std::thread(callable, L"abc");
     }
+
+    //template <typename F, typename ... ArgsT>
+    //Foo(F&& callable, ArgsT&& ... args)
+    //{
+    //    
+    //    //std::thread([](typename std::decay<F>::type&& callable, typename std::decay<ArgsT>::type&&... args)
+    //    //{
+    //    //    try
+    //    //    {
+    //    //        std::bind(callable, args...)();
+    //    //    }
+    //    //    catch (...)
+    //    //    {
+    //    //
+    //    //    }
+    //    //    //(*callable)(std::forward<ArgsT>(args)...);
+    //    //}, std::forward<F>(callable), std::forward<ArgsT>(args)...);
+    //}
 };
 
+void function(std::wstring input, int SomeTing)
+{
+    std::wcout << L"Running" << std::endl;
+    std::wcout << L"Number: " << SomeTing << std::endl;
+    //param->String = input;
+}
 
+template <typename F>
+void ThreadTemplateFunction(F&& callable)
+{
+    std::thread([&](F&& Fuction) 
+    {
+        std::bind(callable, L"abc");
+    }, callable);
+}
+
+void SimplePrint(std::wstring input)
+{
+    std::wcout << L"Some Print: " << input << std::endl;
+}
 
 int main()
 {
     _setmode(_fileno(stdout), _O_U16TEXT);
 
-    Foo FooTing(&function, L"Input", 2);
-    wprintf(FooTing.String.c_str());
+    ThreadTemplateFunction(&SimplePrint);
+
+    //Foo FooTing(&SimplePrint);
+    //wprintf(FooTing.String.c_str());
     return 0;
 }
