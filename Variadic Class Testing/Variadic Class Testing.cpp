751387a46f30747ef2f3ae102419a8a45ff42f32
Noscka <Adam882200@gmail.com>
2022-10-12 20:05:39 +0100

    Working but is using .join(), so its still blocking

--- Variadic Class Testing/Variadic Class Testing.cpp
+++ Variadic Class Testing/Variadic Class Testing.cpp
@@ -5,70 +5,51 @@
 #include <thread>
 #include <functional>
 
-template <typename F, typename ... ArgsT>
-void StartThread(F&& callable, ArgsT&& ... args)
-{
-    (*callable)(this, std::forward<ArgsT>(args)...);
-}
-
+/* For calling in class */
 class Foo
 {
 public:
-    std::wstring String;
-
+  /* just function, compiling error */
     template <typename F>
-    Foo(F* callable)
+    Foo(F& callable)
     {
-        std::thread(callable, L"abc");
+        /* First try */
+        std::thread namedThread(callable, 2);
+        namedThread.join();
+
+        /* Second try (both aren't here at once btw) */
+        //std::thread namedThread([&](F&& Fuction) { std::bind(callable, 2); }, callable);
+        //namedThread.detach();
     }
 
-    //template <typename F, typename ... ArgsT>
-    //Foo(F&& callable, ArgsT&& ... args)
-    //{
-    //    
-    //    //std::thread([](typename std::decay<F>::type&& callable, typename std::decay<ArgsT>::type&&... args)
-    //    //{
-    //    //    try
-    //    //    {
-    //    //        std::bind(callable, args...)();
-    //    //    }
-    //    //    catch (...)
-    //    //    {
-    //    //
-    //    //    }
-    //    //    //(*callable)(std::forward<ArgsT>(args)...);
-    //    //}, std::forward<F>(callable), std::forward<ArgsT>(args)...);
-    //}
+    /* Trying both typename callable with Variadic parameters */
+    template <typename F, typename ... ArgsT>
+    Foo(F&& callable, ArgsT&& ... args)
+    {
+        //std::thread namedThread([&](typename std::decay<F>::type&& Fuction, typename std::decay<ArgsT>::type&&... args) { std::bind(Fuction, args...); }, std::forward<F>(callable), std::forward<ArgsT>(args)...);
+        std::thread namedThread(callable, std::forward<ArgsT>(args)...);
+        namedThread.join();
+    }
 };
 
-void function(std::wstring input, int SomeTing)
+void functionToThread(int SomeTing)
 {
     std::wcout << L"Running" << std::endl;
     std::wcout << L"Number: " << SomeTing << std::endl;
-    //param->String = input;
-}
-
-template <typename F>
-void ThreadTemplateFunction(F&& callable)
-{
-    std::thread([&](F&& Fuction) 
-    {
-        std::bind(callable, L"abc");
-    }, callable);
-}
-
-void SimplePrint(std::wstring input)
-{
-    std::wcout << L"Some Print: " << input << std::endl;
+    return;
 }
 
 int main()
 {
     _setmode(_fileno(stdout), _O_U16TEXT);
-
-    ThreadTemplateFunction(&SimplePrint);
-
-    //Foo FooTing(&SimplePrint);
-    //wprintf(FooTing.String.c_str());
+    //Foo FooTing1(functionToThread);
+    Foo FooTing2(&functionToThread, 3);
     return 0;
 }
+
+
+//template <typename F, typename ... ArgsT>
+//void StartThread(F&& callable, ArgsT&& ... args)
+//{
+//    (*callable)(this, std::forward<ArgsT>(args)...);
+//}
